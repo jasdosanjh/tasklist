a49355393a397e30f3df9020ae9fcf9135268042
jazdosanjh <jaspinder@live.co.uk>
2019-01-09 13:44:40 +0000

    Created logic to add tasks to the list

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,36 @@
-// UI Vars
+// UI Variables
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+function loadEventListeners() {
+    form.addEventListener('submit', addTask);
+}
+
+// Add Task
+function addTask(e) {
+    if (taskInput.value === '') {
+        alert('Add a task');
+    }
+    // Create li element
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // Create new link element
+    const link = document.createElement('a');
+    link.className = 'delete-item secondary-content';
+    // Add icon HTML
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // Append the link to li
+    li.appendChild(link);
+    taskList.appendChild(li);
+    // Clear input
+    taskInput.value = '';
+    e.preventDefault();
+}
